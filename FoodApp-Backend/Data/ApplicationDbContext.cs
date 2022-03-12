84ae36452c96261deb95d4c3421e71fd03e210b1
Patryk Zareba <patrykz19@gmail.com>
2022-03-12 19:03:43 +0100

    Another fix attempt

--- FoodApp-Backend/Data/ApplicationDbContext.cs
+++ FoodApp-Backend/Data/ApplicationDbContext.cs
@@ -10,13 +10,12 @@ namespace FoodApp_Backend.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var server = Environment.GetEnvironmentVariable("DB_SERVER");
-            var port = Environment.GetEnvironmentVariable("DB_PORT");
             var username = Environment.GetEnvironmentVariable("DB_USER");
             var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
             var name = Environment.GetEnvironmentVariable("DB_NAME");
 
             
-            var connectionString = $"Server={server},{port};Initial Catalog={name};User ID={username};Password={password}";
+            var connectionString = $"Server={server};Initial Catalog={name};User ID={username};Password={password}";
 
             optionsBuilder.UseSqlServer(connectionString);
 
